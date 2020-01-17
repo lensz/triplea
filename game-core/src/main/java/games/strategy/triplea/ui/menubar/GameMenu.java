08e1a95334caea9df0a37263c117795d63f74659
Simon Lenz <simonl@topdesk.com>
2020-01-17 17:24:00 +0100

    add example graph with xchart

--- game-core/src/main/java/games/strategy/triplea/ui/menubar/GameMenu.java
+++ game-core/src/main/java/games/strategy/triplea/ui/menubar/GameMenu.java
@@ -30,6 +30,11 @@ import javax.swing.JTextField;
 import javax.swing.KeyStroke;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
+
+import org.knowm.xchart.QuickChart;
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XChartPanel;
+import org.knowm.xchart.XYChart;
 import org.triplea.sound.SoundOptions;
 import org.triplea.swing.IntTextField;
 import org.triplea.swing.SwingAction;
@@ -295,12 +300,17 @@ final class GameMenu extends JMenu {
   }
 
   private void addStatistics() {
-    add(
-        SwingAction.of(
-            "Game statistics",
-            e -> {
-              JOptionPane.showMessageDialog(
-                  frame, null, "Game statistics222", JOptionPane.INFORMATION_MESSAGE);
-            }));
+    add(SwingAction.of(
+        "Game statistics",
+        e -> {
+            double[] xData = new double[]{0.0, 1.0, 2.0};
+            double[] yData = new double[]{2.0, 1.0, 40.0};
+    
+            XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", xData, yData);
+    
+            XChartPanel<XYChart> chartPanel = new XChartPanel<>(chart);
+            JOptionPane.showMessageDialog(frame, chartPanel, "Game statistics222", JOptionPane.INFORMATION_MESSAGE);
+        })
+    );
   }
 }
